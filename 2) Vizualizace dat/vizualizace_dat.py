1d3ef76c6d9f33f10ad7f14f6d402bf4564c4632
ngochuyvu <128366057+NgocHuyVu@users.noreply.github.com>
2023-09-03 00:54:38 +0200

    Add files via upload

--- 2) Vizualizace dat/vizualizace_dat.py
+++ 2) Vizualizace dat/vizualizace_dat.py
@@ -9,7 +9,7 @@ data = pd.read_csv("Data.csv")
 # Graf 1: Top 10 střelců s nejvíce góly
 top10_scorers = data.sort_values(by="Goals", ascending=False).head(10)
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Jméno hráče", y="Góly", data=top10_scorers, palette="viridis")
+sns.barplot(x="Player Names", y="Goals", data=top10_scorers, palette="viridis")
 plt.xlabel("Jméno hráče")
 plt.ylabel("Počet gólů")
 plt.title("Top 10 střelců s nejvíce góly")
@@ -17,22 +17,10 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# Graf 2: Top 10 hráčů má nejlepší poměr gólů/xG
-data["Poměr gólů k xG"] = data["Góly"] / data["xG"]
-top10_scorers_conversion = data.sort_values(by="Poměr gólů k xG", ascending=False).head(10)
-plt.figure(figsize=(10, 6))
-sns.barplot(x="Jméno hráče", y="Poměr gólů k xG", data=top10_scorers_conversion, palette="viridis")
-plt.xlabel("Jméno hráče")
-plt.ylabel("Poměr gólů k xG")
-plt.title("Top 10 hráčů má nejlepší poměr gólů/xG")
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
-
-# Graf 3: Top 10 hráčů s nejvyšším xG
+# Graf 2: Top 10 hráčů s nejvyšším xG
 top10_xG_scorers = data.sort_values(by="xG", ascending=False).head(10)
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Jméno hráče", y="xG", data=top10_xG_scorers, palette="viridis")
+sns.barplot(x="Player Names", y="xG", data=top10_xG_scorers, palette="viridis")
 plt.xlabel("Jméno hráče")
 plt.ylabel("xG (Očekávané góly)")
 plt.title("Top 10 hráčů s nejvyšším xG")
@@ -40,12 +28,26 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
+
+# Graf 3: Top 10 hráčů má nejlepší poměr gólů/xG
+data["Goal Conversion Rate"] = data["Goals"] / data["xG"]
+top10_scorers_conversion = data.sort_values(by="Goal Conversion Rate", ascending=False).head(10)
+plt.figure(figsize=(10, 6))
+sns.barplot(x="Player Names", y="Goal Conversion Rate", data=top10_scorers_conversion, palette="viridis")
+plt.xlabel("Jméno hráče")
+plt.ylabel("Poměr gólů k xG")
+plt.title("Top 10 hráčů má nejlepší poměr gólů/xG")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
+
 # Graf 4: Top ligy podle počtu gólů
-celkové_góly_podle_ligy = data.groupby("Liga")["Góly"].sum().reset_index()
-top_ligy = celkové_góly_podle_ligy.sort_values(by="Góly", ascending=False).head(10)
+total_goals_by_league = data.groupby("League")["Goals"].sum().reset_index()
+top_leagues = total_goals_by_league.sort_values(by="Goals", ascending=False).head(10)
 
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Liga", y="Góly", data=top_ligy, palette="viridis")
+sns.barplot(x="League", y="Goals", data=top_leagues, palette="viridis")
 plt.xlabel("Název ligy")
 plt.ylabel("Celkový počet gólů")
 plt.title("Top ligy podle počtu gólů")
@@ -53,11 +55,12 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# Graf 5: Top ligy podle xG
-celkové_xG_podle_ligy = data.groupby("Liga")["xG"].sum().reset_index()
-top_ligy = celkové_xG_podle_ligy.sort_values(by="xG", ascending=False).head(10)
+
+# Graf 5: Top ligy podle celkového očekávaného počtu gólů (xG)
+total_xG_by_league = data.groupby("League")["xG"].sum().reset_index()
+top_leagues = total_xG_by_league.sort_values(by="xG", ascending=False).head(10)
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Liga", y="xG", data=top_ligy, palette="viridis")
+sns.barplot(x="League", y="xG", data=top_leagues, palette="viridis")
 plt.xlabel("Název ligy")
 plt.ylabel("Celkové xG")
 plt.title("Top ligy podle celkového xG")
@@ -65,12 +68,13 @@ plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
 
-# Graf 6: Top ligy podle poměru gólů/xG
-data["Poměr gólů k xG"] = data["Góly"] / data["xG"]
-poměr_gólů_k_xG_podle_ligy = data.groupby("Liga")["Poměr gólů k xG"].mean().reset_index()
-top_ligy = poměr_gólů_k_xG_podle_ligy.sort_values(by="Poměr gólů k xG", ascending=False).head(10)
+
+# Graf 6: Top ligy podle průměrného poměru gólů k očekávaným gólům (xG)
+data["Goal Conversion Rate"] = data["Goals"] / data["xG"]
+conversion_rate_by_league = data.groupby("League")["Goal Conversion Rate"].mean().reset_index()
+top_leagues = conversion_rate_by_league.sort_values(by="Goal Conversion Rate", ascending=False).head(10)
 plt.figure(figsize=(10, 6))
-sns.barplot(x="Liga", y="Poměr gólů k xG", data=top_ligy, palette="viridis")
+sns.barplot(x="League", y="Goal Conversion Rate", data=top_leagues, palette="viridis")
 plt.xlabel("Název ligy")
 plt.ylabel("Poměr gólů k xG")
 plt.title("Top ligy podle poměru gólů/xG")
